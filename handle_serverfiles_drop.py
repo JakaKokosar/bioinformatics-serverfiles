b5122d0124e833e7ae8d12a6b1a8e7747402c0d4
JakaKokosar <jaka.kokosar@gmail.com>
2019-08-07 14:03:23 +0200

    make GET request not POST (:

--- handle_serverfiles_drop.py
+++ handle_serverfiles_drop.py
@@ -3,7 +3,7 @@ import requests
 
 header = {'Authorization': f'Token {str(sys.argv[1])}'}
 url = f'http://service.biolab.si/serverfiles_update/bioinformatics/{str(sys.argv[2])}'
-response = requests.post(url, headers=header)
+response = requests.get(url, headers=header)
 
 # Fail build if service.biolab.si unavailable
 response.raise_for_status()
